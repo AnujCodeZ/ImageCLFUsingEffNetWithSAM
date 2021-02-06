b295a9806937c27d8ce42f3fb7621e5afda0c54d
AnujCodeZ <anujrana.sid55@gmail.com>
2021-02-06 22:04:15 +0530

    SAM Sharpness Aware Minimization

---
+++ EfficientNetSAM/sam.py
@@ -0,0 +1,34 @@
+import torch
+
+
+class SAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+        super().__init__()
+        self.base_optimizer= base_optimizer
+        self.param_groups = self.base_optimizer.param_groups
+    
+    @torch.no_grad()
+    def step(self):
+        grad_norm = self._grad_norm()
+        for g in self.param_groups:
+            scale = g['rho'] / (grad_norm + 1e-12)
+            for p in g['params']:
+                if p.grad is None: continue
+                e_w = p.grad * scale.to(p)
+                p.add_(e_w)
+        
+        self.base_optimizer.step()
+        
+        self.zero_grad()
+    
+    def _grad_norm(self):
+        norm = torch.norm(
+            torch.stack([
+                p.grad.norm(p=2)
+                for g in self.param_groups
+                for p in g['params']
+                if p.grad is not None
+            ]),
+            p=2
+        )
+        return norm
